1eaeccfd843c01d71085bf81ff3c4dd5beb63f37
itsnotismailov <ismailov@gmail.com>
2022-01-21 14:58:08 +0600

    libraries loaded

--- shareme_backend/schemas/comment.js
+++ shareme_backend/schemas/comment.js
@@ -1,17 +1,17 @@
-export default{
-    name: "comment",
-    title: "Comment",
-    type: "document",
+export default {
+    name: 'comment',
+    title: 'Comment',
+    type: 'document',
     fields: [
-        {
-            name: "postedBy",
-            title: "PostedBy",
-            type: "postedBy"
-        },
-        {
-            name:"comment",
-            title: "Comment",
-            type: "string"
-        },
-    ]
-}
+      {
+        name: 'postedBy',
+        title: 'PostedBy',
+        type: 'postedBy',
+      },
+      {
+        name: 'comment',
+        title: 'Comment',
+        type: 'string',
+      },
+    ],
+  };
